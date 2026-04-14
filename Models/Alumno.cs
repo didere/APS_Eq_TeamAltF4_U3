715e5c8899baacf22d82b5bb413b12f9f269c203
didere <travesurabb@gmail.com>
2026-04-14 08:52:03 -0600

    Agregar archivos de proyecto.

---
+++ Models/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS_Eq_TeamAltF4_U3.Models
+{
+    public class Alumno
+    {
+        private long matricula;
+        private string nombre;
+
+        public long Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public Alumno(long matricula, string nombre)
+        {
+            Matricula = matricula;
+            Nombre = nombre;
+        }
+        public override string ToString()
+        {
+            return "(" + Matricula.ToString() + ", " + Nombre + ")";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using APS_Eq_TeamAltF4_U3.Runners;
+public class Program
+{ 
+    public static void Main(string[] args)
+    {
+        //new Runner01_ArchivoAlumno();
+        new Runner02_RegistroAlumnos();
+
+        //Elaborar un programa que permita un registro de comidas
+        // Elaborar un programa que permita el registro de mascotas
+        //Elaborar un programa qye permita el registro de videojuegos
+        //elaborar un programa que permita el registro de pasatiempos
+        //Elaborar un programa que permita el registro de deportes
+        //cada uno debe de guardar 5 cosas       
+    }
+}
